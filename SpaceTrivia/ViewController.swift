7bbe8d834ffa3d4f3764491b6c0c918190545152
Andrea Sanna <athlons91@gmail.com>
2021-04-22 10:45:40 +0100

    Initial Commit

---
+++ SpaceTrivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpaceTrivia
+//
+//  Created by Athlosn90 on 22/04/2021.
+//  Copyright © 2021 AndreaSanna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
